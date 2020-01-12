e7b68b85b910468b059161b611fc2fd07c8bacab
alexander857 <00355519@uca.edu.sv>
2020-01-11 20:21:56 -0600

    Add: main menu

--- PIZZAIR.cpp
+++ PIZZAIR.cpp
@@ -8,6 +8,7 @@
 using namespace std;
 
 //prototipos de las funciones
+char PizzaMenu();
 
 
 int main(){
@@ -24,8 +25,10 @@ int main(){
 		
 		//se compara si la contrase?a es correcta
 		if(strcmp(password,key) == 0){ //compara las dos palabras: password y key 
-			cout<<"Correcto"<<endl;
-			break;
+			
+			system("cls"); 
+			PizzaMenu(); //se llama la funcion del menu principal si la contraseñña es correcta
+		
 		}
 		
 		else{     //si no son iguaes las palabras, le sigue pidiendo que ingrese la clave hasta que ingrese la correcta
@@ -39,4 +42,21 @@ int main(){
 	}
 	
 	return 0;
+}
+
+//menu principal del sistema de despacho
+char PizzaMenu(){
+	//variables de la funcion
+	char opcion;
+	
+	//nosbre de la pizzaria
+	cout<<"PIZZAIR\n"<<endl;
+	
+	//menu de opciones
+	cout<<"1-Agregar 1 pedido a domicilio\n2-Agregar 1 encargo en restaurante\n3-Ver pedidos a domicilio\n4-Ver encargos en restaurante\n5-Ver total de ventas\n"<<endl;
+	
+	//se pide una opcion a seleccionar
+	cout<<"Opcion: ";cin>>opcion;
+	
+	return opcion;
 }
