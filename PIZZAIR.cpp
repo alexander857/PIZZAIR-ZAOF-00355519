2c2551abf53d345874c295db4361bdaffaf8008e
Alex777 <oalex8393@gmail.com>
2020-01-18 13:46:45 -0600

    Add: function OrderHomeDelibery

--- PIZZAIR.cpp
+++ PIZZAIR.cpp
@@ -116,3 +116,40 @@ char PizzaMenu(){
 		
 	}while(follow);
 }
+
+//funcion donde se piden las ordenes a domicilio
+void OrderHomeDelivery(){
+	
+    if(NumberOfOrders < 5){
+        // Solicitar datos al usuario
+        cout << "Nombre del Cliente: ";
+        getline(cin, OrderList[NumberOfOrders].name);
+        cout << "Direccion: \n";
+        cin >> OrderList[NumberOfOrders].Address.HouseNumber;
+        cout << "Colonia: ";
+        cin >> OrderList[NumberOfOrders].Address.Suburb;
+        cout << "Municipio: ";
+        cin >> OrderList[NumberOfOrders].Address.Municipality;
+        cout << "Departamento: ";
+        cin >> OrderList[NumberOfOrders].Address.State;
+        cout << "\nTelefono: ";
+        cin >> OrderList[NumberOfOrders].phone;
+        cout << "Plato principal: ";
+        cin >> OrderList[NumberOfOrders].MainDish;
+        cout << "Entrada: ";
+        cin >> OrderList[NumberOfOrders].StarterPlate;
+        cout << "Bebida: ";
+        cin >> OrderList[NumberOfOrders].drink;
+        cout << "Monto: $";
+        cin >> OrderList[NumberOfOrders].amount;
+        cout << "Tipo de pago: ";
+        cin >> OrderList[NumberOfOrders].PaymentType;
+        
+        cin.ignore();
+        
+        // Aumentar contador de cantidad de libros
+        NumberOfOrders++;
+    }
+    else cout << "Error, maximo de pedidos!\n";
+         
+}
